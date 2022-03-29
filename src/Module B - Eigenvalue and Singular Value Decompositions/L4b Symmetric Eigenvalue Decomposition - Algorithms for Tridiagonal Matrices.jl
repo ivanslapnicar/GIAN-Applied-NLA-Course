4ca0ed3f0059357fc1dda000e5619727fceefa9c
ivanslapnicar <ivan.slapnicar@fesb.hr>
2022-03-29 18:10:15 +0200

    L4b

--- src/Module B - Eigenvalue and Singular Value Decompositions/L4b Symmetric Eigenvalue Decomposition - Algorithms for Tridiagonal Matrices.jl
+++ src/Module B - Eigenvalue and Singular Value Decompositions/L4b Symmetric Eigenvalue Decomposition - Algorithms for Tridiagonal Matrices.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.14.4
+# v0.17.3
 
 using Markdown
 using InteractiveUtils
@@ -202,23 +202,24 @@ md"
 "
 
 # ╔═╡ e1288577-8a5b-4b21-8533-68be1619a5e6
-# Our matrix
-T
+# Our matrix with positive 	off-diagonal
+T₀=SymTridiagonal(T.dv,abs.(T.ev))
+
+# ╔═╡ a13052ab-297a-4e5a-8654-e65e1fde2e41
+eigvals(T)-eigvals(T₀)
 
 # ╔═╡ 04206e97-57c2-41fe-938f-9108cab56740
 begin
-	T₁=T[1:3,1:3]
-	T₂=T[4:6,4:6]
-	α=T[3,4]
+	T₁=T₀[1:3,1:3]
+	T₂=T₀[4:6,4:6]
+	α=T₀[3,4]
 	T₁[3,3]-=α
 	T₂[1,1]-=α
 	D₁,X₁=eigen(Matrix(T₁))
 	D₂,X₂=eigen(Matrix(T₂))
 	x=zeros(6)
-	x[3]=sqrt(α)
-	x[4]=sqrt(α)
-	# Just for checking
-	T-cat(T₁,T₂,dims=(1,2))-x*x'
+	x[3]=√(α)
+	x[4]=√(α)
 	# X=[X₁ zeros(3,3);zeros(3,3) X₂]
 	X=cat(X₁,X₂,dims=(1,2))
 end
@@ -227,13 +228,19 @@ end
 Matrix(T₂)
 
 # ╔═╡ e2c2803c-e709-43c1-9b79-524bf6dc03e3
-X'*T*X
+X'*T₀*X
 
 # ╔═╡ 6b848adb-1b45-44c8-9be2-e451ef5d21d7
 v=X'*x
 
 # ╔═╡ 2abbf839-847a-4d94-8a4b-f25f57a91540
-Diagonal([D₁;D₂])+v*v'
+D₀=Diagonal([D₁;D₂])+v*v'
+
+# ╔═╡ 903e7308-e7be-4821-82e5-105aca75aca2
+eigvals(D₀)
+
+# ╔═╡ c000c311-8262-4cdf-ace6-13106a792aad
+eigvals(T₀)
 
 # ╔═╡ 574c1653-a2c2-4365-a768-e447bab2d805
 md"
@@ -400,6 +407,223 @@ LAPACK.stegr!('V',copy(T.dv),copy(T.ev))
 # Timings
 @time LAPACK.stegr!('V',copy(T₃.dv),copy(T₃.ev));
 
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+LinearAlgebra = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+Random = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[compat]
+PlutoUI = "~0.7.38"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[[deps.AbstractPlutoDingetjes]]
+deps = ["Pkg"]
+git-tree-sha1 = "8eaf9f1b4921132a4cff3f36a1d9ba923b14a481"
+uuid = "6e696c72-6542-2067-7265-42206c756150"
+version = "1.1.4"
+
+[[deps.ArgTools]]
+uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+
+[[deps.Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+
+[[deps.Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+
+[[deps.ColorTypes]]
+deps = ["FixedPointNumbers", "Random"]
+git-tree-sha1 = "024fe24d83e4a5bf5fc80501a314ce0d1aa35597"
+uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
+version = "0.11.0"
+
+[[deps.CompilerSupportLibraries_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+
+[[deps.Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[deps.Downloads]]
+deps = ["ArgTools", "LibCURL", "NetworkOptions"]
+uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+
+[[deps.FixedPointNumbers]]
+deps = ["Statistics"]
+git-tree-sha1 = "335bfdceacc84c5cdf16aadc768aa5ddfc5383cc"
+uuid = "53c48c17-4a7d-5ca2-90c5-79b7896eea93"
+version = "0.8.4"
+
+[[deps.Hyperscript]]
+deps = ["Test"]
+git-tree-sha1 = "8d511d5b81240fc8e6802386302675bdf47737b9"
+uuid = "47d2ed2b-36de-50cf-bf87-49c2cf4b8b91"
+version = "0.0.4"
+
+[[deps.HypertextLiteral]]
+git-tree-sha1 = "2b078b5a615c6c0396c77810d92ee8c6f470d238"
+uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+version = "0.9.3"
+
+[[deps.IOCapture]]
+deps = ["Logging", "Random"]
+git-tree-sha1 = "f7be53659ab06ddc986428d3a9dcc95f6fa6705a"
+uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
+version = "0.2.2"
+
+[[deps.InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[deps.JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "3c837543ddb02250ef42f4738347454f95079d4e"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.3"
+
+[[deps.LibCURL]]
+deps = ["LibCURL_jll", "MozillaCACerts_jll"]
+uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+
+[[deps.LibCURL_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
+uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+
+[[deps.LibGit2]]
+deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+
+[[deps.LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+
+[[deps.Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+
+[[deps.LinearAlgebra]]
+deps = ["Libdl", "libblastrampoline_jll"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+
+[[deps.Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[deps.Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[deps.MbedTLS_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+
+[[deps.Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
+[[deps.MozillaCACerts_jll]]
+uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+
+[[deps.NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+
+[[deps.OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+
+[[deps.Parsers]]
+deps = ["Dates"]
+git-tree-sha1 = "85b5da0fa43588c75bb1ff986493443f821c70b7"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.2.3"
+
+[[deps.Pkg]]
+deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+
+[[deps.PlutoUI]]
+deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "UUIDs"]
+git-tree-sha1 = "670e559e5c8e191ded66fa9ea89c97f10376bb4c"
+uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+version = "0.7.38"
+
+[[deps.Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[deps.REPL]]
+deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
+uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
+[[deps.Random]]
+deps = ["SHA", "Serialization"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[deps.Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[deps.SHA]]
+uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+
+[[deps.Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+
+[[deps.Sockets]]
+uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
+
+[[deps.SparseArrays]]
+deps = ["LinearAlgebra", "Random"]
+uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+
+[[deps.Statistics]]
+deps = ["LinearAlgebra", "SparseArrays"]
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+
+[[deps.TOML]]
+deps = ["Dates"]
+uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+
+[[deps.Tar]]
+deps = ["ArgTools", "SHA"]
+uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+
+[[deps.Test]]
+deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
+uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+
+[[deps.UUIDs]]
+deps = ["Random", "SHA"]
+uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+
+[[deps.Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+
+[[deps.Zlib_jll]]
+deps = ["Libdl"]
+uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+
+[[deps.libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+
+[[deps.nghttp2_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+
+[[deps.p7zip_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+"""
+
 # ╔═╡ Cell order:
 # ╟─56fea874-cdd8-46c2-babf-10c5bd6440d2
 # ╟─fee10c7e-f1f4-41d4-86a9-c980b5b9b8a5
@@ -424,11 +648,14 @@ LAPACK.stegr!('V',copy(T.dv),copy(T.ev))
 # ╟─9582de91-3ac0-40c8-a160-a52b2d785e2c
 # ╟─131ae1b7-a946-4e3f-b260-d037ee7ee6ce
 # ╠═e1288577-8a5b-4b21-8533-68be1619a5e6
-# ╠═06c885f9-f73c-448e-9ba7-7664ba1ad105
+# ╠═a13052ab-297a-4e5a-8654-e65e1fde2e41
 # ╠═04206e97-57c2-41fe-938f-9108cab56740
+# ╠═06c885f9-f73c-448e-9ba7-7664ba1ad105
 # ╠═e2c2803c-e709-43c1-9b79-524bf6dc03e3
 # ╠═6b848adb-1b45-44c8-9be2-e451ef5d21d7
 # ╠═2abbf839-847a-4d94-8a4b-f25f57a91540
+# ╠═903e7308-e7be-4821-82e5-105aca75aca2
+# ╠═c000c311-8262-4cdf-ace6-13106a792aad
 # ╟─574c1653-a2c2-4365-a768-e447bab2d805
 # ╠═cb7ac40c-b1fb-4b67-8a76-6c0df47fb270
 # ╠═bc5ea97a-70ef-4a95-b59e-96790ba0346f
@@ -447,3 +674,5 @@ LAPACK.stegr!('V',copy(T.dv),copy(T.ev))
 # ╠═145aae34-7b07-421c-aee7-40091783f7a9
 # ╠═02e88a87-da41-4fe0-9cde-457a325b0f74
 # ╠═fd9c471e-aa0b-413e-83f3-2f63b02eb3c4
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
